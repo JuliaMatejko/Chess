583d3dde0158602949a62c0c39a895cace29bf2e
Julia Matejko <matejkojulia@gmail.com>
2021-04-13 02:09:58 +0200

    Diagonal king movement works

--- Chess/Model/Pieces/King.cs
+++ Chess/Model/Pieces/King.cs
@@ -18,8 +18,8 @@ namespace Chess.Model.Pieces
         {
             //type of moves: vertical, horizontal, diagonal, not in line of attack,special: castle
             VerticalKingMove(positions, fileIndex, rankIndex, newField, board);
-            //HorizontalKingMove(positions, fileIndex, rankIndex, field, board);
-            //DiagonalKingMove(positions, fileIndex, rankIndex, field, board);
+            //HorizontalKingMove(positions, fileIndex, rankIndex, newField, board);
+            DiagonalKingMove(positions, fileIndex, rankIndex, newField, board);
             //Move validation! not in line of attack();
             //CastleKingRookMove(); czy powinien byc w tej klasie?
 
@@ -29,9 +29,124 @@ namespace Chess.Model.Pieces
 
         List<string> DiagonalKingMove(List<string> positions, int fileIndex, int rankIndex, Field newField, Board board)
         {
-
+            if (rankIndex < Board.boardSize - 1)
+            {
+                if (IsWhite)
+                {
+                    if (fileIndex == 0)
+                    {
+                        MoveOneForwardDiagonallyRight();
+                    }
+                    if (fileIndex == 7)
+                    {
+                        MoveOneForwardDiagonallyLeft();
+                    }
+                    else
+                    {
+                        MoveOneForwardDiagonallyRight();
+                        MoveOneForwardDiagonallyLeft();
+                    }
+                }
+                else
+                {
+                    if (fileIndex == 0)
+                    {
+                        MoveOneBackwardsDiagonallyLeft();
+                    }
+                    if (fileIndex == 7)
+                    {
+                        MoveOneBackwardsDiagonallyRight();
+                    }
+                    else
+                    {
+                        MoveOneBackwardsDiagonallyLeft();
+                        MoveOneBackwardsDiagonallyRight();
+                    }
+                }
+                if (rankIndex > 0)
+                {
+                    if (IsWhite)
+                    {
+                        if (fileIndex == 0)
+                        {
+                            MoveOneBackwardsDiagonallyRight();
+                        }
+                        if (fileIndex == 7)
+                        {
+                            MoveOneBackwardsDiagonallyLeft();
+                        }
+                        else
+                        {
+                            MoveOneBackwardsDiagonallyLeft();
+                            MoveOneBackwardsDiagonallyRight();
+                        }
+                    }
+                    else
+                    {
+                        if (fileIndex == 0)
+                        {
+                            MoveOneForwardDiagonallyLeft();
+                        }
+                        if (fileIndex == 7)
+                        {
+                            MoveOneForwardDiagonallyRight();
+                        }
+                        else
+                        {
+                            MoveOneForwardDiagonallyRight();
+                            MoveOneForwardDiagonallyLeft();
+                        }
+                    }
+                }
+            }
             return positions;
-        }
+
+
+            void MoveOneForwardDiagonallyLeft() // from the 'piece position of view' - black moves right on the board view
+            {
+                int x = IsWhite ? -1 : 1;
+                int y = IsWhite ? 1 : -1;
+                MoveDiagonally(x, y);
+            }
+
+            void MoveOneBackwardsDiagonallyLeft() // from the 'piece position of view' - black moves right on the board view
+            {
+                int x = IsWhite ? -1 : 1;
+                int y = IsWhite ? -1 : 1;
+                MoveDiagonally(x, y);
+            }
+
+            void MoveOneForwardDiagonallyRight() // from the 'piece position of view' - black moves left on the board view
+            {
+                int x = IsWhite ? 1 : -1;
+                int y = IsWhite ? 1 : -1;
+                MoveDiagonally(x, y);
+            }
+
+            void MoveOneBackwardsDiagonallyRight() // from the 'piece position of view' - black moves left on the board view
+            {
+                int x = IsWhite ? 1 : -1;
+                int y = IsWhite ? -1 : 1;
+                MoveDiagonally(x, y);
+            }
+
+            void MoveDiagonally(int x, int y) // wykorzystać funkcję do napisania bardziej ogólnej? (wektor) TODO
+            {
+                newField = board[fileIndex + x][rankIndex + y];
+                if (newField.Content == null)
+                {
+                    positions.Add(Board.Files[fileIndex + x] + Board.Ranks[rankIndex + y]);
+                }
+                if (newField.Content != null && newField.Content.GetType() != typeof(King))
+                {
+                    bool z = IsWhite ? !(newField.Content.IsWhite) : newField.Content.IsWhite;
+                    if (z)
+                    {
+                        positions.Add(Board.Files[fileIndex + x] + Board.Ranks[rankIndex + y]);
+                    }
+                }
+            }
+    }
 
         List<string> HorizontalKingMove(List<string> positions, int fileIndex, int rankIndex, Field newField, Board board)
         {
@@ -74,7 +189,11 @@ namespace Chess.Model.Pieces
                 }
                 if (newField.Content != null && newField.Content.GetType() != typeof(King))
                 {
-                    positions.Add(Board.Files[fileIndex] + Board.Ranks[rankIndex + y]);
+                    bool z = IsWhite ? !(newField.Content.IsWhite) : newField.Content.IsWhite;
+                    if (z)
+                    {
+                        positions.Add(Board.Files[fileIndex] + Board.Ranks[rankIndex + y]);
+                    }
                 }
             }
 
@@ -88,7 +207,11 @@ namespace Chess.Model.Pieces
                 }
                 if (newField.Content != null && newField.Content.GetType() != typeof(King))
                 {
-                    positions.Add(Board.Files[fileIndex] + Board.Ranks[rankIndex + y]);
+                    bool z = IsWhite ? !(newField.Content.IsWhite) : newField.Content.IsWhite;
+                    if (z)
+                    {
+                        positions.Add(Board.Files[fileIndex] + Board.Ranks[rankIndex + y]);
+                    }
                 }
             }
         }
